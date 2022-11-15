05c667a90b7abce15c19db38978f32a4d1795f59
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2022-11-15 14:04:45 +0200

    test 2

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ let b = "";
 let sign = "";
 let finish = false;
 
-const digits = [];
+const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const action = ["-", "+", "X", "/"];
+
+const out = document.querySelector(".calc-scrin p");
